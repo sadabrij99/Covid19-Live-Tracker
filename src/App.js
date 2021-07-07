f8d2ce0e52d3637b0d27f2f43314d473255fb12a
Sadabrij Shah <83874409+sadabrij99@users.noreply.github.com>
2021-07-07 21:31:42 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Chart from './components/Chart'
 import Covid from './components/covid'
 
 const App =()=> {
     return (
-        <>
-         <Covid/>  
-        </>
+        <div>
+         <Covid/>
+         
+        </div>
     )
 }
 
--- src/components/covid.js
+++ src/components/covid.js
@@ -1,5 +1,6 @@
 import React,{useEffect,useState} from 'react'
 import './ui.css'
+import axios from 'axios'
 
 const Covid = () => {
     //for catch api call data
@@ -9,82 +10,120 @@ const Covid = () => {
 
     // For API call
     useEffect(() => {
-        return async()=>{
-            try {
-                const res = await fetch('https://api.covid19india.org/data.json')
-                //for actual data by calling api
-                const actualData = await res.json();
-                console.log(actualData.statewise[0])
-                // this line assign actual data in data(useState)
-                setData(actualData.statewise[0])
-            } catch (error) {
-               console.log(error) 
-            }
-        }           
+       const covidData = async() =>{
+         try {
+            const {data} = await axios.get('https://api.covid19india.org/data.json')
+            setData(data.statewise[0])
+            
+        } catch (error) {
+           console.log(error) 
+        }
+       }
+        covidData()             
     }, [])
 
 
     
     return (
-        <>
+        <div>
         <div className="project-title" >
            <p>COVID-19 TRACKER INDIA</p>
         </div>
+        <div className="container">
+            <div className="row">
 
-        <div className="card-container" style={{background:'#00FFFF'}}>
-            <div className="card-body">
-                <span className="font">our</span>COUNTRY
-            </div>
-            <div className="card-item">
-                 <p>INDIA</p>
+
+
+               <div className="col">
+                 <div className="card-container mx-auto my-5" style={{background:'#fc9803'}}>
+                    <div className="card-body">
+                      <span className="font">our</span>COUNTRY
+                    </div>
+                    <div className="card-item">
+                       <p>INDIA</p>
+                    </div>
+                  </div>
+               </div>
+
+
+
+
+               <div className="col">
+                  <div className="card-container mx-auto my-5" style={{background:'#7493a3'}}>
+                     <div className="card-body">
+                         <span className="font">total</span>RECOVERED
+                     </div>
+                     <div className="card-item">
+                        <p>{data.recovered}</p>
+                     </div>
+                   </div>
+               </div>
+
+
+
+
+               <div className="col">
+                   <div className="card-container mx-auto my-5" style={{background:'#3399ff'}}>
+                       <div className="card-body">
+                         <span className="font">total</span>CONFIRMED
+                      </div>
+                      <div className="card-item">
+                        <p>{data.confirmed}</p>
+                      </div>
+                  </div>
+               </div>
+
+
+
+
+               <div className="col">
+                  <div className="card-container mx-auto my-5" style={{background:'#696969'}} >
+                      <div className="card-body">
+                         <span className="font">total</span>DEATH
+                      </div>
+                      <div className="card-item">
+                         <p>{data.deaths}</p>
+                      </div>
+                  </div>
+               </div>
+
+
+
+               <div className="col">
+                    <div className="card-container mx-auto my-5" style={{background:'red'}}>
+                        <div className="card-body">
+                           <span className="font">total</span>ACTIVE
+                       </div>
+                       <div className="card-item">
+                          <p>{data.active}</p>
+                      </div>
+                  </div>
+               </div>
+
+
+
+               <div className="col">  
+                  <div className="card-container mx-auto my-5" style={{background:'	#134d00'}}>
+                     <div className="card-body">
+                        <span className="font">last</span>UPDATE
+                     </div>
+                     <div className="card-item" >
+                        <p>{data.lastupdatedtime}</p>
+                     </div>
+                  </div>
+               </div>
+
+
+
             </div>
         </div>
+        
+        <div className="footer">
+            Created by Sada<span style={{color:'red'}}>brij</span> shah @2021
+       </div>
+        
 
-        <div className="card-container" style={{background:'#008080'}}>
-            <div className="card-body">
-             <span className="font">total</span>RECOVERED
-            </div>
-            <div className="card-item">
-                 <p>{data.recovered}</p>
-            </div>
         </div>
-
-        <div className="card-container" style={{background:'#008b8b'}}>
-            <div className="card-body">
-                 <span className="font">total</span>CONFIRMED
-            </div>
-            <div className="card-item">
-                 <p>{data.confirmed}</p>
-            </div>
-        </div>
-
-        <div className="card-container" style={{background:'#696969'}} >
-            <div className="card-body">
-                <span className="font">total</span>DEATH
-            </div>
-            <div className="card-item">
-                 <p>{data.deaths}</p>
-            </div>
-        </div>
-
-        <div className="card-container" style={{background:'red'}}>
-            <div className="card-body">
-              <span className="font">total</span>ACTIVE
-            </div>
-            <div className="card-item">
-                 <p>{data.active}</p>
-            </div>
-        </div>
-
-        <div className="card-container" style={{background:'#708090'}}>
-            <div className="card-body">
-             <span className="font">last</span>UPDATE
-            </div>
-            <div className="card-item" >
-                 <p>{data.lastupdatedtime}</p>
-            </div>
-        </div>
-        </>
     )
 }
 
